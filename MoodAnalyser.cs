cd945b7cdcbb7b27ea8812bafa065dd012bbc592
Pratiksurya1 <suryawanshipratik356@gmail.com>
2022-01-29 10:07:55 +0530

    Add project files.

---
+++ MoodAnalyser.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day22_MoodAnalyser
+{
+    internal class MoodAnalyser
+    {
+        public String AnalyzedMood(String message)
+        {
+            if (message.ToLower().Contains("happy"))
+                return "happy";
+            else if (message.ToLower().Contains("sad"))
+                return "sad";
+            else
+                return "give valid message";
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day22_MoodAnalyser
+{
+    internal class Program
+    {
+        public static void Main(String[] args)
+        {
+            MoodAnalyser moodAnalyser = new MoodAnalyser();
+            Console.WriteLine("how is your mood today !");
+            String message=Console.ReadLine();
+            string actual = moodAnalyser.AnalyzedMood(message);
+            Console.WriteLine(actual);
+        }
+    }
+}
